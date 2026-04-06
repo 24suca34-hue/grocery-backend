1ce0b8e9b76921546cf23a8e3177b69ea37a517c
Sivadharshana <24suca46@tcarts.in>
2026-04-06 16:52:28 +0530

    Added space

--- src/main/java/com/example/demo/controller/GroceryItemController.java
+++ src/main/java/com/example/demo/controller/GroceryItemController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.GroceryItem;
 import com.example.demo.service.GroceryItemService;
-import jakarta.validation.Valid;
+      import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
